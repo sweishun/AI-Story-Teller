2d3d4aa8d509e4d9a28c55378b2a6897d80771c9
sweishun <139720473+sweishun@users.noreply.github.com>
2024-04-28 17:11:09 -0700

    Update extract_text.py

--- extract_text.py
+++ extract_text.py
@@ -4,6 +4,10 @@ def extract_title(text):
   title = text.split("Step 1:")[1].split("Step 2:")[0].strip()
   return title
 
+# def extract_char(text):
+#     char_contents = text.split("Step 4:")[0].split()
+
+#     return char_names
 
 def extract_page_contents(text):
     """
@@ -16,12 +20,14 @@ def extract_page_contents(text):
         dict: A dictionary with keys as page titles ("Page 1", "Page 2", etc.) and values as the corresponding text content.
     """
     # Split the text at "Page Text" to get the relevant section
-    pages_text = text.split("Step 3:")[1].strip()
+    pages_text = text.split("Step 2:")[1].strip()
 
     # Searching for page numbers
     page_contents = re.findall(r'Page \d+:\s*(.*?)\s*(?=(Page \d+:|$))', pages_text, re.DOTALL)
 
     # Extracting just the contents and strip any extra whitespace
     page_texts = {f"Page {i+1}": content.strip() for i, (content, _) in enumerate(page_contents)}
+
+    # Extracting the characters
     
     return page_texts
